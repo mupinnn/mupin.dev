36b53243a8c8ba6169404b4ce6e7f89a146678aa
Ahmad Muwaffaq <itsmupin@gmail.com>
2024-06-15 22:05:02 +0700

    feat: base layout

--- apps/resume/tailwind.config.mjs
+++ apps/resume/tailwind.config.mjs
@@ -3,4 +3,28 @@ import tailwindConfig from "@mupin.dev/shared/tw-config";
 /** @type {import('tailwindcss').Config} */
 export default {
   presets: [tailwindConfig],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            fontWeight: "500",
+            fontSize: "0.625rem",
+            lineHeight: 1,
+
+            "p:first-child": {
+              marginTop: 0,
+            },
+            "p:last-child": {
+              marginBottom: 0,
+            },
+
+            ul: {
+              paddingLeft: "2rem",
+            },
+          },
+        },
+      },
+    },
+  },
 };
