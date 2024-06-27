8712139caae06bd7fbf766fafc7cd5b49846282a
Ahmad Muwaffaq <itsmupin@gmail.com>
2024-06-27 15:21:06 +0700

    feat(shared): footer and avatar components

---
+++ packages/shared/src/components/footer.tsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <footer className="px-4 py-6 text-center text-[10px]">
+      <p className="text-slate-500 dark:text-slate-300">
+        built and designed with ✨ full of joy ✨ by{" "}
+        <a
+          href="https://github.com/mupinnn/mupin.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline decoration-slate-300 decoration-dashed"
+        >
+          me &lt;3
+        </a>{" "}
+        &copy; {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
+}
--- packages/shared/src/components/index.ts
+++ packages/shared/src/components/index.ts
@@ -1,4 +1,7 @@
 export { default as ThemeSelector } from "./theme-selector";
 export { default as UnderConstruction } from "./under-construction";
+export { default as Avatar } from "./avatar";
+export { default as Footer } from "./footer";
+
 export * from "./ui";
 export * from "./providers";
