6ade2b62c3c55b0f754fce6415803642e2796327
Ahmad Muwaffaq <46535277+mupinnn@users.noreply.github.com>
2024-08-10 21:47:13 +0700

    feat(main): now page (#11)
    * feat: now page content
    
    * feat: velite `lastUpdatedAt` property from git
    
    * fix: forgot to set `lastUpdatedAt` schema property to optional
    
    * fix: type error with non-null assertion
    
    * fix: remove strict build temporarily

---
+++ apps/main/app/[locale]/now/page.tsx
@@ -0,0 +1,34 @@
+import { useTranslations } from "next-intl";
+import { unstable_setRequestLocale } from "next-intl/server";
+import { PageLayout, MDXContent } from "@/components";
+import { getPageBySlug } from "@/content";
+import { notFound } from "next/navigation";
+import { createMetadata } from "@/utils/create-metadata";
+import { formatDate } from "@mupin.dev/shared";
+
+export const metadata = createMetadata({
+  canonical: "about",
+});
+
+export default function NowPage({ params: { locale } }: { params: { locale: string } }) {
+  unstable_setRequestLocale(locale);
+  const t = useTranslations("NowPage");
+  const content = getPageBySlug("now", locale);
+
+  if (!content) notFound();
+
+  return (
+    <PageLayout
+      title={t("title")}
+      subtitle={t("subtitle", {
+        lastUpdatedAt: formatDate(
+          content.lastUpdatedAt,
+          { month: "long", day: "numeric", timeZone: "Asia/Jakarta" },
+          locale
+        ),
+      })}
+    >
+      <MDXContent code={content.body} />
+    </PageLayout>
+  );
+}
