2456c7f4499d2940684c1e7d05ecd48ce2eaf9f4
Ahmad Muwaffaq <itsmupin@gmail.com>
2024-08-10 19:55:20 +0700

    feat: velite `lastUpdatedAt` property from git

--- apps/main/velite.config.ts
+++ apps/main/velite.config.ts
@@ -1,4 +1,26 @@
-import { defineConfig, defineCollection, s } from "velite";
+import { defineConfig, defineCollection, s, defineSchema } from "velite";
+import { exec } from "child_process";
+import { promisify } from "node:util";
+
+const execAsync = promisify(exec);
+const gitTimestamp = defineSchema(() => {
+  return s
+    .custom<string | undefined>(i => i === undefined || typeof i === "string")
+    .transform<string>(async (value, { meta, addIssue }) => {
+      if (value !== null) {
+        addIssue({
+          fatal: false,
+          code: "custom",
+          message: "`s.timestamp()` schema will resolve the value from `git log -1 --format=%cd`",
+        });
+      }
+
+      const { stdout } = await execAsync(`git log -1 --format=%cd ${meta.path}`);
+
+      if (stdout === "") return new Date().toISOString();
+      return new Date(stdout).toISOString();
+    });
+});
 
 const pages = defineCollection({
   name: "Page",
@@ -9,6 +31,7 @@ const pages = defineCollection({
       path: s.path(),
       description: s.string().optional(),
       body: s.mdx(),
+      lastUpdatedAt: gitTimestamp(),
     })
     .transform(data => {
       const splittedPath = data.path.split("/");
