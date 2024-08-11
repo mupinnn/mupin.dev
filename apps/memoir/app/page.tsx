6526ee160951b9f0254d1e5deb09dbc9b53bdc16
Ahmad Muwaffaq <itsmupin@gmail.com>
2024-08-11 20:41:04 +0700

    feat(memoir): adjust post list and revalidation

--- apps/memoir/app/page.tsx
+++ apps/memoir/app/page.tsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-import { ThemeSelector, cn, formatDate } from "@mupin.dev/shared";
+import { ThemeSelector, PostList } from "@mupin.dev/shared";
 import { getAllMemoirs } from "@/lib/notion";
 
 export default async function Home() {
@@ -19,40 +18,15 @@ export default async function Home() {
 
       <hr className="text-slate-300 dark:text-slate-500" />
 
-      {allMemoirs.length > 0 ? (
-        <ul className="flex flex-col gap-6">
-          {allMemoirs.map((memoir, i) => (
-            <li
-              className={cn(
-                "relative flex flex-col gap-1",
-                i < allMemoirs.length - 1 &&
-                  "after:absolute after:-bottom-5 after:block after:h-4 after:border-r after:border-dashed after:border-slate-300 after:dark:border-slate-500"
-              )}
-              key={memoir.slug}
-            >
-              <Link href={`/${memoir.slug}`}>
-                <p className="text-xs text-slate-500 dark:text-slate-300">
-                  {formatDate(
-                    memoir.created_at,
-                    {
-                      day: "numeric",
-                      month: "long",
-                      hour: "numeric",
-                      minute: "numeric",
-                      timeZone: "Asia/Jakarta",
-                      timeZoneName: "short",
-                    },
-                    "id-ID"
-                  ).replace("pukul", "-")}
-                </p>
-                <h2 className="font-serif font-semibold">{memoir.title}</h2>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Belum ada apa pun nih . . .</p>
-      )}
+      <PostList
+        data={allMemoirs.map(memoir => ({
+          title: memoir.title,
+          slug: memoir.slug,
+          createdAt: memoir.created_at,
+          locale: "id",
+        }))}
+        emptyMessage="Belum ada apa pun nih . . ."
+      />
     </div>
   );
 }
