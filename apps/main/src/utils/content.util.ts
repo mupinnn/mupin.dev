8515987a7d08ac76e805e6eb0f8eb3abc6fa0adb
Ahmad Muwaffaq <itsmupin@gmail.com>
2025-06-18 17:30:52 +0700

    feat: blog content only locale setup

---
+++ apps/main/src/utils/content.util.ts
@@ -0,0 +1,12 @@
+import { getCollection } from "astro:content";
+import { type Locale } from "@/types";
+
+export async function getEnglishBlogPosts() {
+  const posts = await getCollection("blog");
+  return posts.filter(p => p.data.locale === "en");
+}
+
+export async function getPostTranslations(path: string, locale: Locale) {
+  const posts = await getCollection("blog");
+  return posts.filter(p => p.data.path === path && p.data.locale !== locale);
+}
