87105d331836afe43f27e7dcae138d4616125b2b
Ahmad Muwaffaq <itsmupin@gmail.com>
2025-09-14 12:12:08 +0700

    feat(resume): a revamp (#37)
    * chore(resume): initial setup
    
    * feat: resume content

--- apps/main/src/lib/utils.lib.ts
+++ apps/main/src/lib/utils.lib.ts
@@ -1,25 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { WORKERS_URL } from "@/constant";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-export function formatDate(date: string | Date, options?: Intl.DateTimeFormatOptions) {
-  return new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    weekday: "short",
-    ...options,
-  }).format(new Date(date));
-}
-
-export function getOGImage(title: string) {
-  return `${WORKERS_URL}/og?title=${title}&origin=main`;
-}
-
-export function getWorkersAsset(path: string) {
-  return `${WORKERS_URL}${path}`;
 }
